ac32414e8f7234ba31b984fea22baa98c776f70f
Henrik Gaßmann <BurningEnlightenment@users.noreply.github.com>
2023-08-21 09:34:23 +0200

    fix: Add missing includes
    Don't rely on transitive includes of std ent as they may change at a
    moments notice as has happened with GCC-13 in this case.

--- wg21/file_io_error.cpp
+++ wg21/file_io_error.cpp
@@ -24,6 +24,7 @@ http://www.boost.org/LICENSE_1_0.txt)
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>  // for sprintf
+#include <utility>  // for std::move
 
 #include "status-code/nested_status_code.hpp"
 #include "status-code/system_error2.hpp"
